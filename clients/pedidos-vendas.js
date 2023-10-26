6bb9a6d71e0c745cd04d21bfa0c676315e215d5c
H4ndrycx <handryc2016@gmail.com>
2023-10-26 18:27:31 -0300

    add updateSituacao to PedidosVendaClient

--- clients/pedidos-vendas.js
+++ clients/pedidos-vendas.js
@@ -1,7 +1,17 @@
+const V = require('argument-validator');
 const BaseClient = require('../core/base-client');
 
 class PedidosVendaClient extends BaseClient {
     get endpoint() { return 'pedidos/vendas'; }
+
+    updateSituacao(pedidoId, situacaoId) {
+        V.number(pedidoId, 'pedidoId');
+        V.number(situacaoId, 'situacaoId');
+        return this.doRequest(
+            'PATCH',
+            `${this.endpoint}/${pedidoId}/situacoes/${situacaoId}`
+        );
+    }
 }
 
 module.exports = PedidosVendaClient;
